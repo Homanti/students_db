4e768458e7abe5b5f86f57a497af3c16c2e35157
Vova <homanti0mb2@gmail.com>
2024-05-30 19:42:04 +0200

    first commit

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ while True:
         print(courses_db.get_all_course())
     # Показати список курсів
 
-
     elif choice == "5":
         student_id = int(input("ID Студента "))
         course_id = int(input("ID Курса "))
